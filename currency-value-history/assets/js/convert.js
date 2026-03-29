28ef4ba9d033216f924808e145b07d2582fdbba3
Ryan Snyder <rmsnyder@ualr.edu>
2026-03-29 04:36:13 +0000

    Finished currency converter, edited home page css

--- currency-value-history/assets/js/convert.js
+++ currency-value-history/assets/js/convert.js
@@ -13,77 +13,64 @@ async function convCurrency() {
         let FromDate = document.getElementById("FromDate").value;
         let ToDate = document.getElementById("ToDate").value;
 
+        // Create the forex ticker pair
+        let forexTicker = "C:" + baseCurrency + toCurrency;
+
         /* URL for AJAX Call */
-        let myURL1 = "https://api.massive.com/v3/reference/dividends" + baseCurrency + "?apiKey=" + apiKey;
+        let myURL1 = "https://api.polygon.io/v2/aggs/ticker/" + forexTicker+ "/range/1/day/"+ FromDate+ "/"+ ToDate+ "?adjusted=true&sort=asc&limit=5000&apiKey="+ apiKey;
+
         /* Make the AJAX call */
         let msg1Object = await fetch(myURL1);
+
         /* Check the status */
         if (msg1Object.status >= 200 && msg1Object.status <= 299) {            
             let msg1JSONText = await msg1Object.text();
             // Parse the JSON string into an object
-        }
-        else {
-            /* AJAX complete with error - probably invalid stock ticker symbol */
-                /* Your code to process the result goes here - 
-                   display the returned message */
-            alert("Stock Not Found - Status: " + msg1Object.status)
-            return;
-        }        
- 
-        /* URL for AJAX Call */
-        let myURL2 = "https://api.polygon.io/v2/aggs/ticker/" + StockSymbol + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=false&sort=asc&limit=32&apiKey=" + apiKey;
-        /* Make the AJAX call */
-        let msg2Object = await fetch(myURL2);
-        /* Check the status */
-        if (msg2Object.status >= 200 && msg2Object.status <= 299) {            
-            let msg2JSONText = await msg2Object.text();
-            // Parse the JSON string into an object
-            let msg2 = JSON.parse(msg2JSONText);
+            let msg = JSON.parse(msg1JSONText);
+
             /* Your code to process the result goes here - 
                display the returned message */
-                /* Your code to process the result goes here  
-                    display the returned message */
-                let stockdate = [];
-                let stockvalue = [];
-                let stockvolume = [];
-                let numdays = msg2.results.length;
-                if (numdays > 0) {
-                    for (let i = 0; i < numdays; i++) {
-                        /* stock close value */
-                        stockvalue[i] = msg2.results[i].c;
-                        /* stock volume */
-                        stockvolume[i] = msg2.results[i].v;
-                        /* date is in Unix milleseconds - create a temporary date variable */
-                        let tempdate = new Date(msg2.results[i].t);
-                        /* extract the date string from the value */
-                        stockdate[i] = tempdate.toLocaleDateString();
-                    }
+            let currencyDate = [];
+            let currencyValue = [];
+
+            let numDays = msg.results.length;
+            if (numDays > 0) {
+                for (let i = 0; i < numDays; i++) {
+                    currencyValue[i] = msg.results[i].c;
+
+                    let tempDate = new Date(msg.results[i].t);
+                    currencyDate[i] = tempDate.toLocaleDateString();
                 }
+            }
 
-
-                let ctx0 = document.getElementById("chartjs-0");
-                let myChart0 = new Chart(ctx0, {
+            let ctx = document.getElementById("chartjs");
+                let myChart = new Chart(ctx, {
                     "type":"line",
                     "data": {
-                        "labels": stockdate,
+                        "labels": currencyDate,
                         "datasets":[{"label":"Stock Close",
-                        "data": stockvalue,
+                        "data": currencyValue,
                         "fill":false,
                         "borderColor":"rgb(75, 192, 192)",
-                        "lineTension":0.1}]},
+                        "lineTension":0.1}]
+                    },
                         "options":{ 
                             responsive: false,
                             maintainAspectRatio: true,
                         }
-                    }
-                );         
+                }
+            );         
         }
         else {
-            /* AJAX completed with error - probably invalid stock ticker symbol */
-            alert("Stock Not Found - Status: " + msg2Object.status)
-            return
+            alert("No currency data found for that date range");
         }
     }
+    else {
+            /* Your code to process the result goes here - 
+            display the returned message */
+        alert("Currency Not Found - Status: " + msg1Object.status);
+        return;
+    }        
 }
 
 function ClearForm() {
@@ -104,7 +91,4 @@ function ClearForm() {
     let canvas0 = document.getElementById("chartjs-0");
     let context0 = canvas0.getContext('2d');    
     context0.clearRect(0, 0, canvas0.width, canvas0.height);
-    let canvas1 = document.getElementById("chartjs-1");
-    let context1 = canvas1.getContext('2d');    
-    context1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
