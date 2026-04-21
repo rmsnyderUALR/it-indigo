13f459803a422334534b21c163345be9be96ed73
Ryan Snyder <rmsnyder@ualr.edu>
2026-04-21 19:59:14 +0000

    Filled out homepage of Ponca Project with minimal css

---
+++ ponca-project/assets/js/carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      mobileFirst: true,
+      useAutoplayToggleButton: false
+    });
+
+
+});
